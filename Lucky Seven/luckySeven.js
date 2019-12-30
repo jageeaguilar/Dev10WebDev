c73a38c372cf6708da79af7f50281ab5b191a05e
Josh Aguilar <joshap49@gmail.com>
2019-12-29 19:08:18 -0800

    Finished Lucky Sevens

--- Lucky Seven/luckySeven.js
+++ Lucky Seven/luckySeven.js
@@ -1,33 +1,70 @@
-var diceRolls = 0;
-var maxMoney = userInput;
-var rollsMax = 0;
-var startingBet;
-var gameMoney;
-var dice1;
-var dice2;
 
-function luckySeven(startingBet) {
-	if (startingBet > 0) {
-		gameMoney = startingBet;
+
+function displayResults(startingBet, diceRolls, moneyMax, rollsMax) {	
+	document.getElementById("tableStartingBet").textContent = startingBet;
+	document.getElementById("tableDiceRolls").textContent = diceRolls;
+	document.getElementById("tableMoneyMax").textContent = moneyMax;
+	document.getElementById("tableRollsMax").textContent = rollsMax;
+	document.getElementById("results").style.display = "block";
+
+}
+
+function displayErrorMsg() {
+	document.getElementById("errorMsg").innerHTML = "Please enter a number greater than zero";
+	document.getElementById("errorMsg").style.display = "block";
+}
+
+var bet = document.getElementById("bet");
+
+document.addEventListener("input", function (event) {
+	isValidBet = bet.checkValidity();
+
+	if( !isValidBet ) {
+		displayErrorMsg();
+	}
+}, true);
+
+function getBet() {	
+	var betValue = document.getElementById('bet').value;
+	var startingBet = 0;
+	
+	if(betValue!="" && betValue > 0) {		
+		startingBet = parseInt(betValue);
 	}
 	
-	if (gameMoney > 0){
+	return startingBet;
+}
+
+function luckySeven() {
+	startingBet=getBet();
+	var gameMoney = startingBet;
+	var moneyMax = startingBet;
+	var rollsMax = 0;
+	var diceRolls = 0;
+	
+	while (gameMoney > 0) {
+		var rolls = rollDice() + rollDice();
+		diceRolls++;
+
+		if (rolls == 7) {
+			gameMoney = gameMoney + 4;
+			
+			if (gameMoney > moneyMax) {
+				rollsMax = diceRolls;
+				moneyMax = gameMoney;
+			}
+		}
+		else {
+			gameMoney = gameMoney -1;
+		}
+	}
+	
+	displayResults(startingBet, diceRolls, moneyMax, rollsMax);
+}
 		
 
-function rollDice {
-	dice1 = (Math.floor(Math.random()*6)+1);
-	dice2 = (Math.floor(Math.random()*6)+1);
+function rollDice() {	
+	return Math.floor(Math.random()*6)+1;
+	
 }
 
-function isSeven {
-	if (dice1+dice2===7);
-}
-
-function winLose {
-	if (isSeven){
-		gameMoney = gameMoney + 4;
-	}
-	else {
-		gameMoney--;
-	}
-}
